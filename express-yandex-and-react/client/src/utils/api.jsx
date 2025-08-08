eaee9a6d55926ca9f96a8087729cdf426dfd8050
Evgeny riadesign Pogrebnyak <riadesign@iMac-Evgeny.local>
2025-08-08 18:48:20 +1000

    jr

--- express-yandex-and-react/client/src/utils/api.jsx
+++ express-yandex-and-react/client/src/utils/api.jsx
@@ -20,7 +20,7 @@ api.interceptors.response.use(
   error => {
     if (error.response?.status === 401) {
       localStorage.removeItem('jwt');
-      window.location.href = '/login';
+      window.location.href = '/';
     }
     return Promise.reject(error);
   }
